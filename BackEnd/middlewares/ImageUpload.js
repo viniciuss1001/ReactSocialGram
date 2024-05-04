00b51f55c52b2eb70ff8fc66add45efc99c54901
vinicius2 <viniciussantosfrota10012017@gmail.com>
2024-05-04 18:03:56 -0300

    middleware image upload

---
+++ BackEnd/middlewares/ImageUpload.js
@@ -0,0 +1,34 @@
+const multer = require('multer')
+
+const path = require('path')
+
+//destination to store image
+const imageStorage = multer.diskStorage({
+     destination: function(req, file, callBack){
+          let folder = ''
+
+          if(req.baseUrl.includes('users')){
+              folder = 'users' 
+          }else if(req.baseUrl.includes('photos')){
+               folder = 'photos'
+          }
+          callBack(null, `uploads/${folder}/`)
+     },
+     filename: function(req, file, callBack){
+               callBack(null, Date.now() + path.extname(file.originalname))
+     }
+})
+
+const imageUpload = multer({
+     storage: imageStorage,
+
+     fileFilter(req, file, callBack){
+               if(file.originalname.match(/\.(png|jpg)$/)){
+                    //upload image jpg or png formats
+                    return callBack(new Error('Por favor insira imagens com formato png ou jpg.'))
+               }
+               callBack(undefined, true)
+
+     }
+})
+module.exports = imageUpload
